593a7a649ca7f70a6229ca313a3465713e965039
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-11-14 13:49:41 +0100

    Logo fix

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,10 @@
 /* eslint-env node */
-'use strict';
+'use strict'
 
-const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon')
 
 module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {
-    // Add options here
-  });
+  let app = new EmberAddon(defaults, {})
 
   /*
     This build file specifies the options for the dummy test app of this
@@ -15,5 +13,5 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  return app.toTree();
-};
+  return app.toTree()
+}
