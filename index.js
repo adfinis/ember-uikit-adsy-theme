6c02d5894086f8fa6c973314b0764a56fcdaab27
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-11-20 14:12:53 +0100

    Move base uikit implementation into own addon

--- index.js
+++ index.js
@@ -3,20 +3,9 @@
 
 const Funnel = require('broccoli-funnel');
 const Merge = require('broccoli-merge-trees');
-const buildAstTransform = require('./lib/ast-transform');
 
 module.exports = {
   name: 'ember-uikit-adsy-theme',
-
-  setupPreprocessorRegistry(type, registry) {
-    registry.add('htmlbars-ast-plugin', {
-      name: 'ember-uk-adsy-theme-static-transform',
-      plugin: buildAstTransform(this),
-      baseDir() {
-        return __dirname;
-      }
-    });
-  },
 
   treeForPublic(tree) {
     let firaSans = new Funnel('node_modules/typeface-fira-sans/files', {
@@ -29,23 +18,6 @@ module.exports = {
       destDir: '/assets/files/'
     });
 
-    let uikitImages = new Funnel('node_modules/uikit/src/images', {
-      destDir: '/assets/images/'
-    });
-
-    return new Merge([firaSans, firaMono, uikitImages, tree].filter(Boolean));
-  },
-
-  included(app) {
-    this._super.included.apply(this, arguments);
-
-    app.options.sassOptions = app.options.sassOptions || {};
-    app.options.sassOptions.includePaths =
-      app.options.sassOptions.includePaths || [];
-
-    app.options.sassOptions.includePaths.push('node_modules');
-
-    app.import('node_modules/uikit/dist/js/uikit.min.js');
-    app.import('node_modules/uikit/dist/js/uikit-icons.min.js');
+    return new Merge([firaSans, firaMono, tree].filter(Boolean));
   }
 };
